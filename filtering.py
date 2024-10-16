050dcceac43144106ca9fa7cbc8fa517b5d310a3
Bibas Rai <130794180+bibasrairockz@users.noreply.github.com>
2024-10-17 00:32:27 +0100

    Add files via upload

---
+++ filtering.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+
+img1 = io.imread('DL4CV/w1/golden_retriever.jpg')
+print(img1.shape)
+plt.imshow(img1)
+plt.show()
+
+# Depth
+# Blur
+# Catch change
+# Focus
